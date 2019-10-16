e7eadc07f774885d460fe13ec5316afd107261b5
Zack <43180299+Infihnity3@users.noreply.github.com>
2019-10-16 09:30:22 +0800

    added register php and html file

---
+++ html/register.php
@@ -0,0 +1,27 @@
+<?php
+include "conn.php"; //connnection
+
+//retrieve data from register.html
+$username = mysqli_real_escape_string($conn,$_POST['user']);
+$password = mysqli_real_escape_string($conn,$_POST['pass']);
+$confirmpassword = mysqli_real_escape_string($conn, $_POST['confirmpass']);
+$email = mysqli_real_escape_string($conn,$_POST['email']);
+
+if($password !== $confirmpassword)
+{
+echo "<script>alert('Password and confirmed password not same!'); ";
+die("window.history.go(-1);</script>");
+}
+
+$sql = "Insert into users (User_Name, User_Password, User_Email, User_Last_login ) VALUES ('$username','".md5($password)."','$email','".date("Y-m-d H:i:s")."');";
+mysqli_query($conn, $sql);
+
+//echo $sql;
+if(mysqli_affected_rows($conn)<=0)
+{
+echo "<script>alert('Unable to register ! \\nPlease Try Again!');";
+die("window.history.go(-1);</script>");
+}
+
+echo "<script>alert('Register Successfully!Please login now!');";
+echo "window.location.href='login.html';</script>";
