b807332df7d1e218b5dbd20c065c697801296ad1
Zack <43180299+Infihnity3@users.noreply.github.com>
2019-10-23 16:59:24 +0800

    ..

---
+++ html/index.php
@@ -0,0 +1,72 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Simple Food Inc</title>
+    <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
+    <script src="assets/bootstrap/css/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+    <link rel="stylesheet" href="CSS/index.css" type="text/css">
+</head>
+<body>
+    <!--navbar-->
+    <div class="container-fluid fixed-top">
+            <div class="row">
+                <div class="col-6">
+                    <a class="navbar-logo" href="index.php"><img src="image/logo.png" alt="Simple Food Inc" width="150" height="150" class="txtlogo"></a>
+                </div>
+                <div class="col-6">
+                    <div class="dropdown">
+                        <a href="Register.html">
+                        <button type="button" class="btn btn-outline-dark btn-reg" >
+                        Log In / Register
+                        </button>
+                        </a>
+                    </div>
+                </div>
+            </div>
+        </div>
+    <!-- Join Us Now-->
+    <div class="container-fluid">
+        <div class="col-12"> 
+            <a href="Register.html">
+            <button type="button" class="btn btn-outline-dark btn-join">
+                Join Us Now
+            </button>
+            </a>
+        </div>
+    </div>
+    <!--footer-->
+    <footer class="page-footer blue pt-4">
+            <div class="container-fluid text-center text-md-left">
+                <div class="row">   
+                    <!-- About -->  
+                    <div class="col-4 foot-space"> 
+                      <h5 class="text-uppercase">Simple Food Inc</h5>
+                      <p>San Franciso, California, U.S.A.</p>
+                      <p>+1 234950532</p>
+                      <a href="mailto:simplefoodinc@mail.com"><img src="image/email.png" alt="email us" class="email"></a>
+                    </div>  
+                    <!-- Quick Links -->
+                    <div class="col-4 foot-space">
+                      <h5 class="text-uppercase">Quick Links</h5>
+                      <ul class="list-unstyled">
+                        <li><a href="index.php">Home</a></li>
+                        <li><a href="Register.html">Log In/Register</a></li>
+                      </ul>
+                    </div>
+                    <!-- Enquiry -->
+                    <div class="col-4 foot-space">
+                      <h5 class="text-uppercase">Enquiry</h5>
+                    </div>
+                </div>
+                  <!-- Copyright -->
+            </div>
+            <div class="footer-copyright text-center py-3">© 2019 Copyright by Zack Chow
+            </div>
+        </footer>
+</body>
+</html>
